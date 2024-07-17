e3ac3b14a64669877d074a22f788ec965f228da3
qkenn <github.com.sampling990@passinbox.com>
2024-07-17 08:08:24 +0530

    create random computer move function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('rock');
+function getComputerChoice() {
+  const move = Math.floor(Math.random() * 3) + 1;
+
+  let computerChoice = '';
+
+  switch (true) {
+    case move == 1:
+      computerChoice = 'rock';
+      break;
+    case move == 2:
+      computerChoice = 'paper';
+      break;
+    case move == 3:
+      computerChoice = 'scissors';
+      break;
+  }
+
+  return computerChoice;
+}
+
+console.log(getComputerChoice());
